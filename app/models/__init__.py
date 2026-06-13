763ae86e53c9d3fef48e0975f867f2dbdc042882
Piyush <deshmukhpiyush059@gmail.com>
2026-06-14 01:54:06 +0530

    feat(models): add DocumentChunk model + Alembic migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.workspace import Workspace, WorkspaceMember
 from app.models.document import Document, DocumentStatus
+from app.models.chunk import DocumentChunk  
 
-__all__ = ["User", "Workspace", "WorkspaceMember", "Document", "DocumentStatus"]
+__all__ = ["User", "Workspace", "WorkspaceMember", "Document", "DocumentStatus", "DocumentChunk"]
